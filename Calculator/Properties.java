7c794990ba182f86a288495b3d9ef69938d11c1a
aaron020 <64416980+aaron020@users.noreply.github.com>
2020-12-10 17:24:01 +0000

    Add files via upload

---
+++ Calculator/Properties.java
@@ -0,0 +1,80 @@
+package com.project;
+import java.util.ArrayList;
+
+/**
+ * An arraylist of property objects
+ * Written by Aaron Meade(19271034)
+ */
+public class Properties {
+    private ArrayList<Property> properties;
+    public Properties(){
+        properties = new ArrayList<>();
+    }
+
+    /**
+     * Adds property to the an arraylist
+     * @param p
+     */
+    public void addProperty(Property p){
+        properties.add(p);
+    }
+
+    /**
+     * Adds properties object, used from loading from the CSV file when the program starts
+     * @param other
+     */
+    public void addProperties(Properties other){
+        ArrayList<Property> p = other.getProperties();
+        for(Property n : p){
+            addProperty(n);
+        }
+    }
+
+    /**
+     * Searches all properties for a given name,
+     * returns an Arraylist of properties owned by the specified person
+     * @param o
+     * @return
+     */
+    public ArrayList<Property> searchByName(Owners o){
+        ArrayList<Property> byName = new ArrayList<>();
+        for(Property p : properties){
+            if(p.getOwners().equal(o)){
+                byName.add(p);
+            }
+        }
+        return byName;
+    }
+
+    /**
+     * Arraylist of string with all the unique names
+     * @return
+     */
+    public ArrayList<String> getPropertiesByName(){
+        ArrayList<String> names = new ArrayList<>();
+        for(Property p : properties){
+            names.add(p.getOwners().toString());
+        }
+        ArrayList<String> namesNoRepeat = new ArrayList<>();
+        for(String s:names){
+            if(!namesNoRepeat.contains(s)){
+                namesNoRepeat.add(s);
+            }
+        }
+        return namesNoRepeat;
+    }
+
+    public void changeCalc(double[] newRates,int[] charge){
+        for(Property p : properties){
+            p.changeCalc(newRates,charge);
+        }
+    }
+
+    /**
+     * Returns the properties arraylist
+     * @return
+     */
+    public ArrayList<Property> getProperties() {
+        return properties;
+    }
+}
---
+++ Calculator/Property.java
@@ -0,0 +1,153 @@
+package com.project;
+
+/**
+ * A property object
+ * Written by Aaron Meade(19271034)
+ */
+public class Property {
+    private Owners owners;
+    private String address;
+    private String eircode;
+    private double estMarketVal;
+    private String category;
+    private boolean privateRes;
+    private Calculator c;
+
+    /**
+     * Constructs a Product Object
+     * @param owners The owner of the property.
+     * @param address The address of the property.
+     * @param eircode The eircode of the property.
+     * @param estMarketVal The estimated market value of the property.
+     * @param category The location property of the property.
+     * @param privateRes If the property is a private residence
+     */
+    public Property(Owners owners, String address, String eircode,
+                    double estMarketVal, String category, boolean privateRes) {
+        //Remove all , to stop errors in the CSV
+        address = address.replaceAll(","," ");
+        eircode = eircode.replaceAll(","," ");
+        this.owners = owners;
+        this.address = address;
+        this.eircode = eircode;
+        this.estMarketVal = estMarketVal;
+        this.category = category;
+        this.privateRes = privateRes;
+        c = new Calculator(estMarketVal,category,privateRes);
+    }
+
+    /**
+     * Returns owners
+     * @return String
+     */
+    public Owners getOwners() {
+        return owners;
+    }
+
+    /**
+     * Returns address
+     * @return String
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Returns eircode
+     * @return String
+     */
+    public String getEircode() {
+        return eircode;
+    }
+
+    /**
+     * Returns estMarketVal
+     * @return double
+     */
+    public double getEstMarketVal() {
+        return estMarketVal;
+    }
+
+    /**
+     * Returns category
+     * @return String
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * Returns privateRes
+     * @return boolean
+     */
+    public boolean isPrivateRes() {
+        return privateRes;
+    }
+
+    /**
+     * Returns propertyTax
+     * @return double
+     */
+    public double getPropertyTax() {
+        return this.c.getPropertyTax();
+    }
+
+    public void changeCalc(double[] newRates,int[] charge){
+        c.changeCalc(newRates,charge);
+    }
+
+//    /**
+//     * Calculates the property tax
+//     */
+//    private void calcPropertyTax(){
+//        propertyTax = propertyTax + 100 + Rates() + Categories();
+//        if(!privateRes){
+//            propertyTax = propertyTax  + 100;
+//        }
+//    }
+//
+//    /**
+//     * Calculates rate for property tax
+//     * @return double
+//     */
+//    private double Rates(){
+//        double PropertyTax = 0;
+//        int[] Values = {150_000,400_000,650_000};
+//        double[] Rates = {0,.01,.02,.04};
+//        for(int i = 0; i < Values.length;i++){
+//            if(estMarketVal <= Values[i]){
+//                PropertyTax = PropertyTax + estMarketVal*Rates[i]/100;
+//                return PropertyTax;
+//            }else if(estMarketVal > 650000){
+//                PropertyTax = PropertyTax + estMarketVal*.04/100;
+//                return PropertyTax;
+//            }
+//        }
+//        return PropertyTax;
+//    }
+//
+//    /**
+//     * Calculates property tax based off of category
+//     * @return double
+//     */
+//    private double Categories(){
+//        double PropertyTax = 0;
+//        String[] categories = {"City","Large Town","Small Town","Village","Countryside"};
+//        int[] charge = {100,80,60,50,25};
+//        for(int i = 0; i< categories.length;i++){
+//            if(category.compareToIgnoreCase(categories[i]) == 0){
+//                PropertyTax = PropertyTax + charge[i];
+//            }
+//        }
+//        return PropertyTax;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "Name : "+ owners.toString() + "\n" +"Address: "+ address + "\n"
+                +"Eircode: "+ eircode + "\n"+ "Estimated Market Value: " + estMarketVal + "\n"
+                +"Category: "+ category + "\n" +
+                "Property Tax: " + c.getPropertyTax() + "\n";
+    }
+}
